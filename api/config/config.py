79a2157d1e52ce9831950df08ffbb0d63af61a5c
Santiag0gopenux2024 <michael.martinez@gopenux.com>
2024-09-06 10:09:41 -0500

    Docker-compose funcional y corregido

--- api/config/config.py
+++ api/config/config.py
@@ -2,21 +2,21 @@ import os
 
 config = {
     "app": {
-        "debug": os.getenv("APP_DEBUG", None),
-        "port": os.getenv("APP_PORT", 5000),
+        "debug": os.getenv("APP_DEBUG", "False"),
+        "port": int(os.getenv("APP_PORT", 5000)),
         "host": os.getenv("APP_HOST", "0.0.0.0"),
         "secret_key": os.getenv("SECRET_KEY", "R29wZW51eERldmVsb3Blcgo=")
     },
     "redis": {
         "host": os.getenv("REDIS_HOST", "localhost"),
-        "port": os.getenv("REDIS_PORT", 6379),
+        "port": int(os.getenv("REDIS_PORT", 6379)),
     },
     "mongo": {
         "host": os.getenv("MONGO_HOST", "localhost"),
-        "port": os.getenv("MONGO_PORT", 27017),
+        "port": int(os.getenv("MONGO_PORT", 27017)),
     },
     "image_service": {
         "host": os.getenv("IMAGE_MICROSERVICE_HOST", "localhost"),
-        "port": os.getenv("IMAGE_MICROSERVICE_PORT", 3001),
+        "port": int(os.getenv("IMAGE_MICROSERVICE_PORT", 3001)),
     }
-}
+} 
